76cacfce834f6e3b920fca32f9e80b09457d611a
Петр Петров <3033106@gmail.com>
2022-04-09 18:27:31 +0400

    changes files

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,8 @@ import java.util.zip.ZipOutputStream;
 
 public class Main {
 
+    private static String slash = File.separator;
+    private static String gamesSave = "Games" + slash + "savegames" + slash;
 
     private static void saveGame(GameProgress progress, String path) {
         try(FileOutputStream fosProgress  = new FileOutputStream(path);
@@ -25,9 +27,11 @@ public class Main {
             for (String s : pathFile) {
 
                 try (FileInputStream fis = new FileInputStream(s)) {
-                    String[] lastPath = s.split("/");
 
-                    ZipEntry entry = new ZipEntry(lastPath[lastPath.length - 1]);
+                    int lastIndexSlash = s.lastIndexOf(slash);
+
+
+                    ZipEntry entry = new ZipEntry(s.substring(lastIndexSlash + 1));
                     zout.putNextEntry(entry);
 
                     byte[] buffer = new byte[fis.available()];
@@ -49,20 +53,20 @@ public class Main {
 
     public static void main(String[] args) {
 
-        File saveDir = new File("Games/savegames");
+        File saveDir = new File("Games" + slash + "savegames");
 
         GameProgress player1 = new GameProgress(82,3, 32, 54.2);
         GameProgress player2 = new GameProgress(76,1, 15, 10.4);
         GameProgress player3 = new GameProgress(100,9, 80, 102.7);
 
-        saveGame(player1, "Games/savegames/save1.dat");
-        saveGame(player2, "Games/savegames/save2.dat");
-        saveGame(player3, "Games/savegames/save3.dat");
+        saveGame(player1, gamesSave + "save1.dat");
+        saveGame(player2, gamesSave + "save2.dat");
+        saveGame(player3, gamesSave + "save3.dat");
 
-        zipFiles("Games/savegames/zip.zip",
-                "Games/savegames/save1.dat",
-                "Games/savegames/save2.dat",
-                "Games/savegames/save3.dat");
+        zipFiles(gamesSave + "zip.zip",
+                gamesSave + "save1.dat",
+                gamesSave + "save2.dat",
+                gamesSave + "save3.dat");
 
         for (File item : saveDir.listFiles()){
             if (!item.getName().equals("zip.zip")) item.delete();
